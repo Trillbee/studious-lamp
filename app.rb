dca2343d0c95af3ef3252b0b8ae1fb48318f3da6
Trillbee <adelaide@MacBook-Air.local>
2017-01-19 12:05:41 +1100

    attempt simple add of hypdf functionality

--- app.rb
+++ app.rb
@@ -33,3 +33,22 @@ get "/create" do
   dashboard_url << "apps/#{match[1]}/resources" if match && match[1]
   redirect to(dashboard_url)
 end
+
+def pdfunite
+  file_1 = params[:file_1]
+  file_2 = params[:file_2]
+  options = {test: true}
+
+  if params[:commit] == 'Download PDF'
+    hypdf = HyPDF.pdfunite(file_1.path, file_2.path, options)
+    send_data(hypdf[:pdf], filename: 'hypdf_test.pdf', type: 'application/pdf')
+  else
+    options.merge!(
+      bucket: 'agtesthypdf', #'hypdf_test', # NOTE: replace 'hypdf_test' with your backet name
+      key: 'hypdf_test.pdf',
+      public: true
+    )
+    hypdf = HyPDF.pdfunite(file_1.path, file_2.path, options)
+    redirect_to '/pdfunite', notice: "PDF url: #{hypdf[:url]}"
+  end
+end
