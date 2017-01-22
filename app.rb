bddc0e6a8f1eba24a1e77658c1bf6f2cd252a245
Trillbee <adelaide@MacBook-Air.local>
2017-01-23 09:20:55 +1100

    map/show account and attachment tables

--- app.rb
+++ app.rb
@@ -4,9 +4,6 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 require 'hypdf'
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "rails/all"
 
 class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
@@ -14,6 +11,16 @@ end
 
 get "/contacts" do
   @contacts = Contact.all
+  erb :index
+end
+
+get "/accounts" do
+  @accounts = Account.all
+  erb :index
+end
+
+get "/attachments" do
+  @attachments = Attachment.all
   erb :index
 end
 
@@ -26,6 +33,13 @@ class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
 end
 
+class Account < ActiveRecord::Base
+  self.table_name = 'salesforce.account'
+end
+
+class Attachment < ActiveRecord::Base
+  self.table_name = 'salesforce.attachment'
+end
 #get "/contacts" do
 #  @contacts = Contact.all
 #  erb :index
@@ -39,26 +53,9 @@ get "/create" do
 end
 
 get "/pdfunite" do
-  @hypdf = HyPDF.htmltopdf(
-    '<html><body><h1>Title</h1></body></html>',
-    orientation: 'Landscape',
-    copies: 2,
-    # ... other options ...
-)
-  # file_1 = params[:file_1]
-  # file_2 = params[:file_2]
-  # options = {test: true}
-  #
-  # if params[:commit] == 'Download PDF'
-  #   hypdf = HyPDF.pdfunite(file_1.path, file_2.path, options)
-  #   send_data(hypdf[:pdf], filename: 'hypdf_test.pdf', type: 'application/pdf')
-  # else
-  #   options.merge!(
-  #     bucket: 'agtesthypdf', #'hypdf_test', # NOTE: replace 'hypdf_test' with your backet name
-  #     key: 'hypdf_test.pdf',
-  #     public: true
-  #   )
-  #   hypdf = HyPDF.pdfunite(file_1.path, file_2.path, options)
-  #   redirect_to '/pdfunite', notice: "PDF url: #{hypdf[:url]}"
-  # end
+  file_1 = params[:file_1]
+  file_2 = params[:file_2]
+  options = {test: true}
+  hypdf = HyPDF.pdfunite(file_1.path, file_2.path, options)
+  send_data(hypdf[:pdf], filename: 'hypdf_test.pdf', type: 'application/pdf')
 end
