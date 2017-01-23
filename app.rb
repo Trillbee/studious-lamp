ef4483bf73ce0e4ea28465c45967d9351b5501ea
Trillbee <adelaide@MacBook-Air.local>
2017-01-23 15:58:07 +1100

    updated gemfile for hypdf

--- app.rb
+++ app.rb
@@ -94,9 +94,9 @@ end
 
 post "/pdfunite" do
 
-  file_1 = params[:file][:tempfile]
-  file_2 = params[:file][:tempfile]
+  file_1 = params[:file]
+  file_2 = file_1
   options = {test: true}
-  hypdf = HyPDF.pdfunite(file_1, file_2, options)
+  hypdf = HyPDF.pdfunite(file_1.path, file_2.path, options)
   send_data(hypdf[:pdf], filename: 'hypdf_test.pdf', type: 'application/pdf')
 end
