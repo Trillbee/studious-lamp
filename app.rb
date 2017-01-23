00296979d846196047b7a4186487839ba15efbdd
Trillbee <adelaide@MacBook-Air.local>
2017-01-24 09:12:24 +1100

    merging desktop files and saving to Amazon

--- app.rb
+++ app.rb
@@ -40,16 +40,6 @@ get "/upload_image" do
   erb :form
 end
 
-
-# class Contact < ActiveRecord::Base
-#   self.table_name = 'salesforce.contact'
-# end
-
-#get "/contacts" do
-#  @contacts = Contact.all
-#  erb :index
-#end
-
 get "/create" do
   dashboard_url = 'https://dashboard.heroku.com/'
   match = /(.*?)\.herokuapp\.com/.match(request.host)
@@ -69,33 +59,8 @@ post '/save_image' do
   erb :show_image
 end
 
-# get "/pdfunite" do
-#   erb :pdfunite
-# end
-#
-# get "/unite" do
-#   file_1 = params[:file_1]
-#   file_2 = params[:file_2]
-#   options = {test: true}
-#
-#   if params[:commit] == 'Download PDF'
-#     hypdf = HyPDF.pdfunite(file_1.path, file_2.path, options)
-#     send_data(hypdf[:pdf], filename: 'hypdf_test.pdf', type: 'application/pdf')
-#   else
-#     options.merge!(
-#       bucket: 'agtesthypdf', #'hypdf_test', # NOTE: replace 'hypdf_test' with your backet name
-#       key: 'hypdf_test.pdf',
-#       public: true
-#     )
-#     hypdf = HyPDF.pdfunite(file_1.path, file_2.path, options)
-#     redirect_to '/pdfunite', notice: "PDF url: #{hypdf[:url]}"
-#   end
-# end
-
 post "/pdfunite" do
 
-  # file_1 = JSON.parse(params[:file_1].to_json)
-  # file_2 = JSON.parse(params[:file_2].to_json)
   file_1 = params[:file_1][:tempfile]
   file_2 = params[:file_2][:tempfile]
 
@@ -108,10 +73,5 @@ post "/pdfunite" do
   hypdf = HyPDF.pdfunite(file_1, file_2, options)
 
   erb :pdfunite
-  # redirect_to '/pdfunite', notice: "PDF url: #{hypdf[:url]}"
-  # options = {test: true}
-  #
-  # hypdf = HyPDF.pdfunite(file_1, file_2, options)
 
-  # send_data(hypdf[:pdf], filename: 'hypdf_test.pdf', type: 'application/pdf')
 end
