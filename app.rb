d766d71751810bfaab80282f4e627923c19d72e9
Trillbee <adelaide@MacBook-Air.local>
2017-01-24 11:01:17 +1100

    get 2 random SF PDFs and merge

--- app.rb
+++ app.rb
@@ -82,13 +82,13 @@ get "/sfpdfunite" do
   @file_1 = Attachment.where("contenttype= 'application/pdf'").limit(1)
   @file_2 = Attachment.where("contenttype= 'application/pdf'").limit(1)
 
-  # options = {
-  #   test: true,
-  #   bucket: 'agtesthypdf',
-  #   key: 'SFhypdf_test.pdf',
-  #   public: true
-  # }
-  # hypdf = HyPDF.pdfunite(file_1, file_2, options)
+  options = {
+    test: true,
+    bucket: 'agtesthypdf',
+    key: 'SFhypdf_test.pdf',
+    public: true
+  }
+  hypdf = HyPDF.pdfunite(@file_1, @file_2, options)
 
   erb :pdfunite
 
