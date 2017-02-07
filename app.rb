915dfdb27611c4ba6dc12fcc06824ea67bf867b7
Trillbee <sethuraman_a@listertechnologies.com>
2017-02-07 11:38:09 +0530

    sethu: hex unpack

--- app.rb
+++ app.rb
@@ -89,8 +89,10 @@ get "/sfpdfunite" do
   # print encoded_file_1.inspect
   # print encoded_file_2.inspect
   
-  file1 = encoded_file_1_hex.scan(/../).map { |x| x.hex.chr }.join
-  file2 = encoded_file_2_hex.scan(/../).map { |x| x.hex.chr }.join
+  # file1 = encoded_file_1_hex.scan(/../).map { |x| x.hex.chr }.join
+  # file2 = encoded_file_2_hex.scan(/../).map { |x| x.hex.chr }.join
+  
+  file1 = encoded_file_1_hex.unpack('A')
   
   #file1 = Base64.decode64(encoded_file_1)
   #file2 = Base64.decode64(encoded_file_2)
