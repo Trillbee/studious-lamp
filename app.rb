c9b3a1d79cdccaf1948ab44e9c4d796908ddea61
Trillbee <sethuraman_a@listertechnologies.com>
2017-02-08 09:35:09 +0530

    sethu: testing v2

--- app.rb
+++ app.rb
@@ -80,28 +80,11 @@ end
 
 get "/sfpdfunite" do
 
-  encoded_file_1_hex = Attachment.where("name= 'test pdf 1.pdf'").limit(1).pluck(:Body)[0]
-  #encoded_file_2_hex = Attachment.where("name= 'test pdf 2.pdf'").limit(1).pluck(:Body)[0]
+  file1_base64_string = Attachment.where("name= 'test pdf 1.pdf'").limit(1).pluck(:Body)[0]
   
-  print encoded_file_1_hex.inspect
+  hypdf = HyPDF.pdfunite(file1_base64_string, file1_base64_string)
   
-  
-  file1 = Base64.decode64( encoded_file_1_hex.scan(/../).map { |x| x.hex }.pack('c*') )
-  #file2 = Base64.decode64( encoded_file_2_hex.scan(/../).map { |x| x.hex }.pack('c*') )
-  
-  options = {
-     test: true,
-     bucket: 'agtesthypdf',
-     key: 'SFhypdf_test.pdf',
-     public: true
-  }
-   
-  #hypdf = HyPDF.pdfunite(file1, file1)
-  #print hypdf.inspect
-
-  #hex_data = Base64.encode64(hypdf[:pdf]).unpack('H*').first
-  
-  #Attachment.create(name: 'SFhypdf_test_2_7.pdf', contenttype: 'application/pdf', parentid: '00628000008AaUnAAK', body: hex_data)
+  print hypdf.inspect
   
   erb :form
 
